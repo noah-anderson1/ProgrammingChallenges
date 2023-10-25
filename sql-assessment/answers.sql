e8bb9eef76c6b9ea6ee29dfd7fbf59f479cdb2fb
noah-anderson1 <148905158+noah-anderson1@users.noreply.github.com>
2023-10-24 22:30:35 -0500

    Update answers.sql

--- sql-assessment/answers.sql
+++ sql-assessment/answers.sql
@@ -1,3 +1,5 @@
+`marketing_performance` contains daily ad spend and performance metrics -- impression, clicks, and conversions -- by campaign_id and location:
+```sql
 create table marketing_data (
  date datetime,
  campaign_id varchar(50),
@@ -7,6 +9,26 @@ create table marketing_data (
  clicks float,
  conversions float
 );
+```
 
+`website_revenue` contains daily website revenue data by campaign_id and state:
+```sql
+create table website_revenue (
+ date datetime,
+ campaign_id varchar(50),
+ state varchar(2),
+ revenue float
+);
+```
+
+`campaign_info` contains attributes for each campaign:
+```sql
+create table campaign_info (
+ id int not null primary key auto_increment,
+ name varchar(50),
+ status varchar(50),
+ last_updated_date datetime
+);
+```
 
 
